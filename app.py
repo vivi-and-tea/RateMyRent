770371df2d6f277532fdf81b68452d930505c75e
vivi-and-tea <vtshore@gmail.com>
2021-09-13 23:09:45 +0900

    Basic layout

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = POSTGRES_ADD
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
+
 class Places(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     city = db.Column(db.String(80), unique=True, nullable=False)
@@ -22,41 +23,71 @@ class Places(db.Model):
     def __repr__(self):
         return '<Cities %r>' % self.city
 
+places = db.session.query(Places).all()
+room_type = ""
 
-# @app.route("/")
-# def home():
-#     return render_template('index.html')
+def rent_finder(place, room_type):
+    if room_type == "oneroom":
+        return place.oneroom
+    elif room_type == "onek":
+        return place.onek
+    elif room_type == "twok":
+        return place.twok
+    elif room_type == "threek":
+        return place.threek
+    elif room_type == "fourk":
+        return place.fourk
+
+def rate_rent(percent):
+    if percent <= 25:
+        return "A+"
+    elif percent <= 30:
+        return "A"
+    elif percent <= 33:
+        return "B+"
+    elif percent <= 35:
+        return "B"
+    elif percent <= 40:
+        return "B-"
+    elif percent <= 45:
+        return "C"
+    elif percent <= 45:
+        return "C-"
+
+
+
 
 @app.route("/")
 def home():
+    return render_template('index.html')
+
+@app.route('/rate')
+def rate():
+
     salary = 24
-    places = db.session.query(Places).all()
-    return render_template('index.html', places=places)
+    room_type = "twok"
+    user_city = 3
+    place = Places.query.get(user_city)
+    rent = rent_finder(place, room_type)
+    percent = int((rent / salary) * 100)
+    grade = rate_rent(percent)
+
+    return render_template('rate.html', place=place, rent=rent, percent=percent, grade=grade)
 
 @app.route("/recommend")
 def recommend():
     salary = 20
     room_type = "onek"
     list = []
-    places= db.session.query(Places).all()
-
-    def place_finder(place):
-        if room_type == "oneroom":
-            return place.oneroom
-        elif room_type == "onek":
-            return place.onek
-        elif room_type == "twok":
-            return place.twok
 
     for place in places:
 
-        the_room = place_finder(place)
-        percent = int((the_room / salary) * 100)
+        rent = rent_finder(place, room_type)
+        percent = int((rent / salary) * 100)
 
 
         if percent < 50 and percent > 20:
-            list.append({'City': place.city, 'Rent': the_room, 'Percent': percent})
-
+            list.append({'City': place.city, 'Rent': rent, 'Percent': percent})
         else:
             pass
 
@@ -71,6 +102,7 @@ def recommend():
     context['places'] = places
     context['room_type'] = room_type
     context['salary'] = salary
+
     if mini:
         context['mini'] = mini
     if message:
